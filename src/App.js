10315ab0f5dcedcbbb3f108a6b3732bda21b3685
DominikColl <DAColl@students.fullsail.edu>
2020-08-03 01:43:15 -0400

    'ff'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/nav'
+import Landing from './components/landing'
+function App() {
+  return (
+    <div className="App">
+     <Nav/>
+     <Landing/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nav.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import Logo from '../imgs/logo.png' 
+class Nav extends Component {
+    render() {
+      return (
+          <nav>
+<div id='navWrap'>
+            <h1>Coll Web Dev</h1>
+
+            <ul id='navList'>
+                <li>Home</li>
+                <li>Mission</li>
+                <li>Client Work</li>
+            </ul>
+            </div>
+            </nav>
+      )
+    }
+  }
+  export default Nav
