e65abb0afb70174052df509edd45022ee91a8821
DominikColl <DAColl@students.fullsail.edu>
2020-08-03 01:51:04 -0400

    Adding missions

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Nav from './components/nav'
 import Landing from './components/landing'
+import Misson from './components/mission'
 function App() {
   return (
     <div className="App">
      <Nav/>
      <Landing/>
+     <Misson/>
     </div>
   );
 }
---
+++ src/components/mission.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+
+class Mission extends Component {
+    render() {
+      return (
+     <section id='missionSection'>
+<h2>Misson</h2>
+<ul>
+    <li>Fast</li>
+    <li>Reliable</li>
+    <li>Mobile</li>
+</ul>
+
+     </section>
+      )
+    }
+  }
+  export default Mission
